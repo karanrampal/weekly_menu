8e2b7f04d5cfc19931a0c0a801d2376a981a9911
Karan Rampal <karan.rampal@ericsson.com>
2019-02-13 16:42:33 +0100

    Add function to write menu in a text file

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ def args_parser():
                         help="Webpage to be scraped")
     return parser.parse_args()
 
+def write_menu(restaurant_name, week_name, titles, descriptions, prices):
+    """Write the menu from the data in the argumntns to a text file
+    Args:
+        restaurant_name: (string) Name of the restaurant
+        week_name: (string) Name of the week
+        titiles: (string) Category of the food
+        descriptions: (string) Description of the food
+        prices: (string) Price of each food
+    """
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+
+    val = 4 if restaurant_name == "EricoFood" else 3
+
+    file_name = restaurant_name + time.strftime("%Y%m%d-%H%M%S") + ".txt"
+    with open(file_name, "w") as f:
+        f.write("{0}\n".format(week_name))
+        for day in weekdays:
+            f.write("{0}\n".format(day))
+            for i in range(val):
+                f.write("{0} {1} {2}\n".format(titles[i], descriptions[i], prices[i]))
+            f.write("\n")
+
 def main():
     """Main function
     """
@@ -27,8 +49,8 @@ def main():
     tree = html.fromstring(page.content)
 
     # Extract html tag information
-    week = tree.xpath('//h2[@class="header-week"]/text()')
-    week = week[0].strip()
+    week_name = tree.xpath('//h2[@class="header-week"]/text()')
+    week_name = week_name[0].strip()
 
     titles = tree.xpath('//div[@class="element title col-md-4 col-print-3"]/text()')
     titles = [x.strip() for x in titles]
@@ -36,21 +58,11 @@ def main():
     descriptions = tree.xpath('//div[@class="element description col-md-4 col-print-5"]/text()')
     prices = tree.xpath('//div[@class="element price col-md-1 col-print-1"]/text()')
 
-    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-
     # Check restaurant name
-    restaurant = "EricoFood" if "ericofood" in args.web_page else "Factory"
-    val = 4 if restaurant == "EricoFood" else 3
+    restaurant_name = "EricoFood" if "ericofood" in args.web_page else "Factory"
 
     # Write in text file
-    file_name = restaurant + time.strftime("%Y%m%d-%H%M%S") + ".txt"
-    with open(file_name, "w") as f:
-        f.write("{0}\n".format(week))
-        for day in weekdays:
-            f.write("{0}\n".format(day))
-            for i in range(val):
-                f.write("{0} {1} {2}\n".format(titles[i], descriptions[i], prices[i]))
-            f.write("\n")
+    write_menu(restaurant_name, week_name, titles, descriptions, prices)
 
 
 if __name__ == '__main__':
