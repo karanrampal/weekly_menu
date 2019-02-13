d5af1daed5dcc0170f7251c200bf5f37dcee7ed8
Karan Rampal <karan.rampal@ericsson.com>
2019-02-13 16:54:21 +0100

    Add function to parse html

--- main.py
+++ main.py
@@ -14,6 +14,26 @@ def args_parser():
     parser = argparse.ArgumentParser()
     return parser.parse_args()
 
+def parse_html(web_page):
+    """Parse the given webpage and extract the desired content such as price, food description
+    Args:
+        web_page: (string) URL of the webpage
+    """
+    # Read the web page and extract the html content
+    page = requests.get(web_page)
+    tree = html.fromstring(page.content)
+
+    # Extract html tag information
+    week_name = tree.xpath('//h2[@class="header-week"]/text()')
+    week_name = week_name[0].strip()
+
+    titles = tree.xpath('//div[@class="element title col-md-4 col-print-3"]/text()')
+    titles = [x.strip() for x in titles]
+
+    descriptions = tree.xpath('//div[@class="element description col-md-4 col-print-5"]/text()')
+    prices = tree.xpath('//div[@class="element price col-md-1 col-print-1"]/text()')
+    return week_name, titles, descriptions, prices
+
 def write_menu(restaurant_name, week_name, titles, descriptions, prices):
     """Write the menu from the data in the argumntns to a text file
     Args:
@@ -42,22 +62,14 @@ def main():
     # Read arguments
     args = args_parser()
 
-    # Read the web page and extract the html content
-    page = requests.get(args.web_page)
-    tree = html.fromstring(page.content)
+    wp1 = "https://ericsson.foodbycoor.se/ericofood/en/weekmenu"
+    wp2 = "https://ericsson.foodbycoor.se/the-factory/en/weekmenu"
 
-    # Extract html tag information
-    week_name = tree.xpath('//h2[@class="header-week"]/text()')
-    week_name = week_name[0].strip()
-
-    titles = tree.xpath('//div[@class="element title col-md-4 col-print-3"]/text()')
-    titles = [x.strip() for x in titles]
-
-    descriptions = tree.xpath('//div[@class="element description col-md-4 col-print-5"]/text()')
-    prices = tree.xpath('//div[@class="element price col-md-1 col-print-1"]/text()')
+    # Extract content from the webpage
+    week_name, titles, descriptions, prices = parse_html(wp1)
 
     # Check restaurant name
-    restaurant_name = "EricoFood" if "ericofood" in args.web_page else "Factory"
+    restaurant_name = "EricoFood" if "ericofood" in wp1 else "Factory"
 
     # Write in text file
     write_menu(restaurant_name, week_name, titles, descriptions, prices)
